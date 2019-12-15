f588c996cc5c20dc531b9bebd3a90147120a37fc
Chun Hei MatthewAu Yeung <1810192@AIV.EDMC.STU>
2019-12-14 21:41:57 -0800

    [Matthew] --- Moved gitignore into Unity file.

---
+++ GameBackend/GameBackend/Database/GetSQLPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using GameBackend.Models;
+
+namespace GameBackend.Database
+{
+    public class GetSQLPlayer : SQLWrapper
+    {
+        public PlayerData playerData { get; set; }
+        protected override void SQLCommands()
+        {
+            string sql = "SELECT first_name, HeadOfState FROM Country WHERE Continent='North America'";
+
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                Console.WriteLine(rdr[0] + "\t" + rdr[1]);
+            }
+            rdr.Close();
+        }
+    }
+}
